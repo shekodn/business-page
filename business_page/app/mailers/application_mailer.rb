642d5e08174688de010748044fa63e302ade8d11
shekodn <sdiazn19@gmail.com>
2016-09-22 21:00:03 -0500

    gitignore password and mail

--- business_page/app/mailers/application_mailer.rb
+++ business_page/app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout 'mailer'
+    default from: "Your Mailer <noreply@yourdomain.com>"
+    default to: "Your Name <your.email@yourdomain.com>"
+
+    def new_message(message)
+        @message = message
+
+        mail subject: "Message from #{message.name}"
+    end
+
 end
--- business_page/config/routes.rb
+++ business_page/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
     root 'static_pages#home'
     get 'logo' => 'static_pages#home'
-    
+
+    get 'contact', to: 'messages#new', as: 'contact'
+    post 'contact', to: 'messages#create'
+
+
+
     resources :static_pages
 end
